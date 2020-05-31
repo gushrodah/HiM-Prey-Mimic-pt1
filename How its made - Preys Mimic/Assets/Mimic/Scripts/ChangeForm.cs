1f4f5c8e9bf00e1f20a005ff616f7102f4fe429b
Wade <wade.gushikuma@gmail.com>
2020-05-31 13:24:23 -0700

    part 1

---
+++ How its made - Preys Mimic/Assets/Mimic/Scripts/ChangeForm.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChangeForm : MonoBehaviour
+{
+	public List<GameObject> ObjectList;
+
+	public bool isDisguised;
+	public UnityEvent OnDisguisedAction, OnReveledAction;
+	public Transform player;
+	public float changeDistance = 2;
+
+	float curDistanceFromPlayer { get { return Vector3.Distance(player.transform.position, transform.position); } }
+	GameObject spawnedObject;
+
+	void Update()
+    {
+		if (curDistanceFromPlayer < changeDistance && isDisguised)
+		{
+			OnReveledAction.Invoke();
+		}
+    }
+
+	public void Disguise()
+	{
+		OnDisguisedAction.Invoke();
+		isDisguised = true;
+	}
+
+	public void Reveal()
+	{
+		OnReveledAction.Invoke();
+		isDisguised = false;
+	}
+
+	public void ChooseAndSpawnObject()
+	{
+		int randomNumber = Random.Range(0, ObjectList.Count);
+		spawnedObject = Instantiate(ObjectList[randomNumber], transform);
+		spawnedObject.transform.localPosition = Vector3.zero;
+		spawnedObject.transform.localRotation = Quaternion.identity;
+	}
+
+	public void DespawnObject()
+	{
+		if (spawnedObject != null)
+		{
+			Destroy(spawnedObject);
+		}
+	}
+
+	public void SetRevealed(bool isRevealed)
+	{
+		isDisguised = !isRevealed;
+	}
+}
---
+++ How its made - Preys Mimic/Assets/Mimic/Scripts/Move.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Move : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	public List< Transform> targetDestination;
+	public UnityEvent OnLocationReached;
+
+	Vector3 targetPosition = Vector3.zero;
+	float distance { get { return Vector3.Distance(targetPosition, transform.position);  } }
+	float changeDistance = 1;
+
+    void Start()
+    {
+		agent = GetComponent<NavMeshAgent>();
+		ChangeLocation();
+    }
+
+
+	bool locationReached = false;
+
+	private void Update()
+	{
+		if (distance < changeDistance && !locationReached)
+		{
+			agent.velocity = Vector3.zero;
+			agent.ResetPath();
+			OnLocationReached.Invoke();
+			locationReached = true;
+			Debug.Log("reeached");
+		}
+		else if(distance > changeDistance)
+		{
+			locationReached = false;
+		}
+	}
+
+	public void ChangeLocation()
+	{
+		int randomNumber = Random.Range(0, targetDestination.Count);
+		targetPosition = targetDestination[randomNumber].position;
+		agent.SetDestination(targetPosition);
+	}
+}
